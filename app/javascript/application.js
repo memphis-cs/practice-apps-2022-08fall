e0cc7d3ddae9b615df59501cbfb8462747459ecc
Katie Bridson <kbridson@memphis.edu>
2022-10-07 00:34:47 -0500

    New Projects Step 5. Add JavaScript Packages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,13 @@ import "controllers"
 
 import "popper"
 import "bootstrap"
+import autosize from "autosize"
 
 document.addEventListener("turbo:load", () => {
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  autosize(document.querySelectorAll('textarea'))
 })
