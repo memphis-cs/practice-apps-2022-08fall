a321ff6e62279ad93f74cbf58dbc239c8d15d6f8
Katie Bridson <kbridson@memphis.edu>
2022-10-07 00:27:55 -0500

    Added flash messages display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  BOOTSTRAP_ALERT_CLASS = {
+    'success' => 'alert-success',
+    'error'   => 'alert-danger',
+    'notice'  => 'alert-info',
+    'alert'   => 'alert-danger',
+    'warn'    => 'alert-warning'
+  }.freeze
+
+  def alert_class(flash_key)
+    BOOTSTRAP_ALERT_CLASS[flash_key]
+  end
 end
